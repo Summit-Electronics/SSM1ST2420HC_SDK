74fbb451c97b25904b348d665ef495ab9d7a80c7
Wesley Goedegebuure <52458417+Wesley-TOP@users.noreply.github.com>
2023-06-19 15:36:52 +0200

    updated gitignore

--- 1_AXIS_F091_GPIO/Debug/Core/Src/subdir.mk
+++ 1_AXIS_F091_GPIO/Debug/Core/Src/subdir.mk
@@ -31,7 +31,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Src/%.o Core/Src/%.su: ../Core/Src/%.c Core/Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F091xC -c -I../Core/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F0xx/Include -I../Drivers/CMSIS/Include -I"C:/Users/wesley/OneDrive - TOP-electronics/STM32CubeIDE/workspace_1.9.0/1_AXIS_F091_GPIO/Core/Inc/tmc/helpers" -I"C:/Users/wesley/OneDrive - TOP-electronics/STM32CubeIDE/workspace_1.9.0/1_AXIS_F091_GPIO/Core/Inc/tmc/ic" -I"C:/Users/wesley/OneDrive - TOP-electronics/STM32CubeIDE/workspace_1.9.0/1_AXIS_F091_GPIO/Core/Inc/tmc/ramp" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F091xC -c -I../Core/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F0xx/Include -I../Drivers/CMSIS/Include -I"C:/Github_Local/SSM1ST2420HC/1_AXIS_F091_GPIO/Core/Inc/tmc/helpers" -I"C:/Github_Local/SSM1ST2420HC/1_AXIS_F091_GPIO/Core/Inc/tmc/ic" -I"C:/Github_Local/SSM1ST2420HC/1_AXIS_F091_GPIO/Core/Inc/tmc/ramp" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Core-2f-Src
 
