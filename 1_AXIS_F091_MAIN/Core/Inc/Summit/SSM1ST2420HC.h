8b7b146962f3544c154514be064342cae80d3e92
Wesley Goedegebuure <52458417+Wesley-TOP@users.noreply.github.com>
2024-02-22 14:23:22 +0100

    added V1.0

---
+++ 1_AXIS_F091_MAIN/Core/Inc/Summit/SSM1ST2420HC.h
@@ -0,0 +1,126 @@
+/*
+ * SSM1ST2420HC.h
+ *
+ *  Created on: 3 nov. 2022
+ *      Author: Wesley Goedegebuure
+ *      Email: wesley@top-electronics.com
+ */
+
+#ifndef INC_SUMMIT_SSM1ST2420HC_H_
+#define INC_SUMMIT_SSM1ST2420HC_H_
+
+#include "AS5055A_Registers.h"
+#include "stm32f0xx_hal.h"
+#include "main.h"
+
+typedef struct {
+	uint32_t VSTART;
+	uint32_t A1;
+	uint32_t V1;
+	uint32_t AMAX;
+	uint32_t VMAX;
+	uint32_t DMAX;
+	uint32_t D1;
+	uint32_t VSTOP;
+} RampConfig;
+
+typedef struct {
+	uint32_t IRUN;
+	uint32_t IHOLD;
+} CurrentConfig;
+
+/* TMC5160 functions */
+void TMC5160_Basic_Init(CurrentConfig *Current);
+/* Perform basic initialize of the TMC5160 and enable the DRV_Enable pin which controls the H-Bridge signals.
+ * IRUN, IHOLD, full scale, see excel sheet for current estimate
+ */
+
+void TMC5160_Basic_Rotate(uint8_t Mode, RampConfig *Ramp);
+/* Start stepper rotation in direction: "mode"
+ * "mode" 0 or 1, 0 = right , 1 = left
+ *  with Rampingprofile as set by RampConfig
+ */
+void TMC5160_Rotate_To(uint32_t Position, RampConfig *Ramp);
+/* Rotate Stepper to "position"
+ * "position": uint32_t (51200 = right, -51200 = left turn)
+ * with Rampingprofile as set by RampConfig
+ */
+void TMC5160_Stop(void);
+/* Stop all movement by writing VMAX = 0;
+ */
+
+void Drive_Enable(int state);
+/* Enables the DRV_ENB pin, powering the motor and disabling free movement of the axis
+ * "state": int (1 = enable drive , 0 = disable drive)
+ */
+
+uint32_t TMC5160_SPIWrite(uint8_t Address, uint32_t Value, int Action);
+/* SPI "action" on register at "Address" for "Value" on the TMC5160
+ * "action" 0 or 1, 0 = read, 1 = write
+ * "address", see TMC5160 folder
+ * "value", uint32_t value for the register
+ */
+uint16_t TMC_Get_Position();
+/* Read the position register on the TMC5160 and convert the result (32 bit) to an angle (Degrees).
+ */
+
+void TMC5160_Init_Stallguard(int reset);
+/* Initialize Stallguard
+ * "reset" = 1 will reset the stall event and allow the motor to move again
+ *
+ * TODO: implement TSTEP, TPWMTHRS, TCOOLTHRS, THIGH needed?
+ *  //TSTEP>- TPWMTHRS  , stealthchop PWM mode is enabled
+ * 	//TCOOLTHRS >- TSTEP , stop on stall is enabled
+ */
+
+int TMC5160_Monitor_Stallguard(void);
+/* monitor stallguard values
+ * returns stallguard Flag value (1 = stall , 0 = no stall)
+ */
+
+void TMC5160_Init_Stealthchop(void);
+/* Initialize Stealthchop
+ */
+
+/* AMS5055 functions */
+void AMS5055_Basic_Init(void);
+/* Wake up the AMS5055 and perform basic initialize, also performs a read angle to calibrate the output of AMS5055_get_Position().
+ * only perform this function once
+ */
+uint16_t AMS5055_Get_Position(void);
+/* Start a READ_ANGLE event and convert the result (12 bit) to an angle (Degrees).
+ * TODO: check reliability
+ */
+uint8_t AMSParity(uint16_t value);
+/* Calculate parity bit for SPI transaction
+ * "value" the SPI transmit message
+ */
+uint16_t AMS5055_SPIWriteInt(uint16_t Address, int Action);
+/* SPI "action" on register at "Address" for AMS5055
+ * "action" 0 or 1, 0 = write , 1 = read
+ * "address" , see AS5055A_registers.h
+ */
+
+/* Encoder functions */
+uint16_t ENC_Get_Position(void);
+/* Read the Encoder value register on TMC5160, and convert the result(32 bit) to an angle (Degrees)
+ * TODO: check reliability
+ */
+
+/* GPIO functions */
+void Toggle_OUT(int port,uint16_t time);
+/* Toggle (24V) Output on "port" for duration of "time"
+ * "port" 1 or 2, respectively OUT1 or OUT2
+ * "time" duration in ms
+ */
+int Read_IN(int port);
+/* Read input (5 to 24V) on "port"
+ * "port" 1 or 2, respectively IN1 or IN2
+ */
+uint16_t Read_AIN(void);
+/* Read analog input, absolute maximum = 5.0V
+ * A voltage divider is used to scale voltage to MCU voltage.
+ * (Input voltage x (15K / 10K+15K)) = voltage on MCU pin
+ */
+
+#endif /* INC_SUMMIT_SSM1ST2420HC_H_ */
---
+++ 1_AXIS_F091_Receive/Core/Src/main.c
@@ -0,0 +1,523 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* CODE
+TMC5160
+- SPI interface
+- GPIO DRV_ENN (Low = Motor On, High = Motor Off)
+- GPIO STEP
+- GPIO DIR
+
+AS5055A
+- SPI interface
+- INT GPIO (optional)
+
+TJA1042TK_3_1J
+- CAN interface (CAN1 SEND 0x01 , if CAN2 receives 0x01 , send back 0x02. if CAN1 receives 0x02 , send back 0x01, repeat)
+- GPIO CAN_STB (Low = Normal , High = Standby)  CHECK
+
+EXT_OUT (24V)
+- GPIO EXT_OUT_1
+- GPIO EXT_OUT_2
+
+EXT_IN (24V tolerant)
+- GPIO REFL_UC
+- GPIO REFR_UC
+
+ADC_IN (5V tolerant)
+- AIN AIN_MCU
+*/
+
+
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ ADC_HandleTypeDef hadc;
+
+CAN_HandleTypeDef hcan;
+
+SPI_HandleTypeDef hspi1;
+SPI_HandleTypeDef hspi2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_ADC_Init(void);
+static void MX_CAN_Init(void);
+static void MX_SPI1_Init(void);
+static void MX_SPI2_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* RAMP config */
+RampConfig Ramp1;
+RampConfig StallSettings1;
+RampConfig StealthSettings1;
+RampConfig StopRamp1;
+
+/* Current config */
+CurrentConfig CurrentSetting1;
+
+/* Settings */
+int AMS_ENB = 0; // 0 = disable Hall sensor , 1 = enable Hall sensor
+int ENC_ENB = 0; // 0 = disable Encoder , 1 = enable Encoder
+int STG_ENB = 0; // 0 = disable Stallguard, 1 = enable Stallguard
+
+/* CAN VARIABLES */  //
+CAN_TxHeaderTypeDef CANTxHeader;
+CAN_RxHeaderTypeDef CANRxHeader;
+
+uint8_t CANTxData[8];	//CANTX data array
+uint8_t CANRxData[8];	//CANRX data array
+uint32_t TxMailbox[3];	//CAN Mailbox
+int Datacheck;			//temp value for checking incomming CAN Data
+
+//int count = 0;
+
+/*  CAN RECEIVE INTERRUPT */
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+	HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &CANRxHeader, CANRxData);
+
+	if (CANRxData[0] == 0x01)
+	{
+		Datacheck = 1;
+	}
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ADC_Init();
+  MX_CAN_Init();
+  MX_SPI1_Init();
+  MX_SPI2_Init();
+  /* USER CODE BEGIN 2 */
+
+  HAL_CAN_Start(&hcan);
+  HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING);
+
+  // DATA pakket opbouw CAN BUS
+
+  CANTxHeader.DLC = 8;	// data length
+  CANTxHeader.ExtId = 0; // ext ID
+  CANTxHeader.IDE = CAN_ID_STD;
+  CANTxHeader.RTR = CAN_RTR_DATA;
+  CANTxHeader.StdId = 0x222; // ID
+  CANTxHeader.TransmitGlobalTime = DISABLE;
+
+  CANTxData[0] = 0x01; // dummy data
+  CANTxData[1] = 0x02; // dummy data
+  CANTxData[2] = 0x03; // dummy data
+  CANTxData[3] = 0x04; // dummy data
+  CANTxData[4] = 0x05; // dummy data
+  CANTxData[5] = 0x06; // dummy data
+  CANTxData[6] = 0x07; // dummy data
+  CANTxData[7] = 0x08; // dummy data
+
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+		if (Datacheck == 1)
+		{
+			Datacheck = 0;
+			if (HAL_CAN_AddTxMessage(&hcan, &CANTxHeader, &CANTxData, &TxMailbox) != HAL_OK)
+			{
+				Error_Handler();
+			}
+		}
+
+	}
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSI14;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSI14State = RCC_HSI14_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.HSI14CalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC_Init(void)
+{
+
+  /* USER CODE BEGIN ADC_Init 0 */
+
+  /* USER CODE END ADC_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC_Init 1 */
+
+  /* USER CODE END ADC_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc.Instance = ADC1;
+  hadc.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc.Init.ScanConvMode = ADC_SCAN_DIRECTION_FORWARD;
+  hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc.Init.LowPowerAutoWait = DISABLE;
+  hadc.Init.LowPowerAutoPowerOff = DISABLE;
+  hadc.Init.ContinuousConvMode = DISABLE;
+  hadc.Init.DiscontinuousConvMode = DISABLE;
+  hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc.Init.DMAContinuousRequests = DISABLE;
+  hadc.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  if (HAL_ADC_Init(&hadc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel to be converted.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC_Init 2 */
+
+  /* USER CODE END ADC_Init 2 */
+
+}
+
+/**
+  * @brief CAN Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_CAN_Init(void)
+{
+
+  /* USER CODE BEGIN CAN_Init 0 */
+	HAL_GPIO_WritePin(GPIOA,CAN_STB_Pin,0); // Set STB pin LOW for normal operation
+  /* USER CODE END CAN_Init 0 */
+
+  /* USER CODE BEGIN CAN_Init 1 */
+
+  /* USER CODE END CAN_Init 1 */
+  hcan.Instance = CAN;
+  hcan.Init.Prescaler = 2;
+  hcan.Init.Mode = CAN_MODE_NORMAL;
+  hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
+  hcan.Init.TimeSeg1 = CAN_BS1_2TQ;
+  hcan.Init.TimeSeg2 = CAN_BS2_1TQ;
+  hcan.Init.TimeTriggeredMode = DISABLE;
+  hcan.Init.AutoBusOff = DISABLE;
+  hcan.Init.AutoWakeUp = DISABLE;
+  hcan.Init.AutoRetransmission = DISABLE;
+  hcan.Init.ReceiveFifoLocked = DISABLE;
+  hcan.Init.TransmitFifoPriority = DISABLE;
+  if (HAL_CAN_Init(&hcan) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN CAN_Init 2 */
+
+
+  /* CAN filter */
+
+  CAN_FilterTypeDef  sFilterConfig;
+
+  sFilterConfig.FilterActivation = ENABLE;
+  sFilterConfig.FilterBank = 10;
+  sFilterConfig.FilterFIFOAssignment = CAN_RX_FIFO0;
+  sFilterConfig.FilterIdHigh = 0x0000;
+  sFilterConfig.FilterIdLow = 0x0000;
+  sFilterConfig.FilterMaskIdHigh = 0x0000;
+  sFilterConfig.FilterMaskIdLow = 0x0000;
+  sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
+  sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
+  sFilterConfig.SlaveStartFilterBank = 0;
+
+  HAL_CAN_ConfigFilter(&hcan, &sFilterConfig);
+
+  HAL_CAN_Start(&hcan);
+  HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING);
+
+  /* USER CODE END CAN_Init 2 */
+
+}
+
+/**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_4BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 7;
+  hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi1.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
+  * @brief SPI2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI2_Init(void)
+{
+
+  /* USER CODE BEGIN SPI2_Init 0 */
+
+  /* USER CODE END SPI2_Init 0 */
+
+  /* USER CODE BEGIN SPI2_Init 1 */
+
+  /* USER CODE END SPI2_Init 1 */
+  /* SPI2 parameter configuration*/
+  hspi2.Instance = SPI2;
+  hspi2.Init.Mode = SPI_MODE_MASTER;
+  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi2.Init.DataSize = SPI_DATASIZE_4BIT;
+  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi2.Init.NSS = SPI_NSS_SOFT;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi2.Init.CRCPolynomial = 7;
+  hspi2.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi2.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+  if (HAL_SPI_Init(&hspi2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI2_Init 2 */
+
+  /* USER CODE END SPI2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, CAN_STB_Pin|DRV_ENN_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, EXT_OUT_1_Pin|EXT_OUT_2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : CAN_STB_Pin DRV_ENN_Pin */
+  GPIO_InitStruct.Pin = CAN_STB_Pin|DRV_ENN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : EXT_OUT_1_Pin EXT_OUT_2_Pin */
+  GPIO_InitStruct.Pin = EXT_OUT_1_Pin|EXT_OUT_2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : REFL_UC_Pin */
+  GPIO_InitStruct.Pin = REFL_UC_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(REFL_UC_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : REFR_UC_Pin */
+  GPIO_InitStruct.Pin = REFR_UC_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(REFR_UC_GPIO_Port, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
