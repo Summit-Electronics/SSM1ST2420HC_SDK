69f57418d626593be90bd52ec8b5ff9190eac982
Wesley Goedegebuure <52458417+Wesley-TOP@users.noreply.github.com>
2024-02-22 14:19:08 +0100

    added SDK versions

--- 1_AXIS_F091_Send/Debug/Core/Startup/startup_stm32f091cctx.d
+++
@@ -1,2 +0,0 @@
-Core/Startup/startup_stm32f091cctx.o: \
- ../Core/Startup/startup_stm32f091cctx.s
--- 1_AXIS_F091_Turntable_Demo/Debug/Core/Inc/tmc/helpers/CRC.d
+++
@@ -1,4 +0,0 @@
-Core/Inc/tmc/helpers/CRC.o: ../Core/Inc/tmc/helpers/CRC.c \
- ../Core/Inc/tmc/helpers/CRC.h ../Core/Inc/tmc/helpers/Types.h
-../Core/Inc/tmc/helpers/CRC.h:
-../Core/Inc/tmc/helpers/Types.h:
